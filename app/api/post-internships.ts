19f56c4a713fa68b240d3b4c44a5b9c8f525e94b
samrath2007 <samrath1324@gmail.com>
2021-09-06 18:14:44 +0530

    add(api): post internship route

---
+++ app/api/post-internships.ts
@@ -0,0 +1,22 @@
+import prisma from "db"
+import { internshipType } from "types"
+
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const data = req.body as internshipType
+  if (
+    !(data.position && data.contract && data.location && data.logo && data.company && data.tools)
+  ) {
+    console.log(data)
+    res.status(400).json({
+      code: "bad-data",
+      message:
+        "The data was either missing a position key, contract key, data location key, company key, or tools key",
+    })
+  }
+  await prisma.internship.create({
+    data: data,
+  })
+  res.status(200).send("Valid Data!!!!")
+}
