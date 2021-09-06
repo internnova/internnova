a23413ea830342365a3a522f0300351d1783c990
samrath2007 <samrath1324@gmail.com>
2021-09-06 11:01:14 +0530

    fix(web): implement responsive fix from #1 in other pages

--- app/auth/components/SignupForm.tsx
+++ app/auth/components/SignupForm.tsx
@@ -16,10 +16,7 @@ export const SignupForm = (props: SignupFormProps) => {
       <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen p-28">
         <img src="/images/AuthHero.svg" alt="" id="twoTimesGif" className="w-full h-full" />
       </div>
-      <div
-        className="bg-white w-full lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-        flex items-center justify-center"
-      >
+      <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
         <div className="w-full h-100">
           <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">Create An Account</h1>
           <Form
