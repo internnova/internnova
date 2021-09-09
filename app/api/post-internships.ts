dcb8ee40a1a2084338dc00f08377c26c6baf3830
samrath2007 <samrath1324@gmail.com>
2021-09-09 18:06:32 +0530

    add(db, api): duration field

--- app/api/post-internships.ts
+++ app/api/post-internships.ts
@@ -4,28 +4,37 @@ import { internshipType } from "types"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = req.body as internshipType
-  if (
-    !(
-      data.position &&
-      data.contract &&
-      data.location &&
-      data.logo &&
-      data.company &&
-      data.tools &&
-      data.description &&
-      data.numOfOpenings
-    )
-  ) {
-    res.status(400).json({
-      code: "bad-data",
-      message:
-        "The data was either missing a logo key, position key, contract key, location key, company key, description key, a number of openings key, or tools key",
+  try {
+    const data = req.body as internshipType
+    if (
+      !(
+        data.position &&
+        data.contract &&
+        data.location &&
+        data.logo &&
+        data.company &&
+        data.tools &&
+        data.description &&
+        data.numOfOpenings &&
+        data.duration
+      )
+    ) {
+      res.status(400).json({
+        code: "bad-data",
+        message:
+          "The data was either missing a logo key, position key, contract key, location key, company key, description key, a number of openings key, or tools key",
+      })
+    }
+    await prisma.internship.create({
+      data: data,
+    })
+    res.status(200).json({
+      code: "success",
+    })
+  } catch {
+    res.status(500).json({
+      code: "internal-server-error",
     })
   }
-  await prisma.internship.create({
-    data: data,
-  })
-  res.status(200).send("Valid Data!!!!")
 }
 export default handler
