d06c3ff8722dc45dcb4c6562aa50144d8d21616c
samrath2007 <samrath1324@gmail.com>
2021-09-10 13:09:12 +0530

    add: auth0 user to database

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,3 +1,23 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import prisma from "db"
+import { handleAuth, handleCallback } from "@auth0/nextjs-auth0"
 
-export default handleAuth();
+const afterCallback = async (req, res, session, state) => {
+  try {
+    await prisma.user.create({
+      data: {
+        name: session.user.name as string,
+        email: session.user.email as string,
+        picture:
+          (session.user.picture as string) ||
+          "https://st4.depositphotos.com/4329009/19956/v/600/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg",
+      },
+    })
+  } catch {}
+  return session
+}
+
+export default handleAuth({
+  async callback(req, res) {
+    await handleCallback(req, res, { afterCallback })
+  },
+})
