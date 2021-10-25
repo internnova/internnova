10dc63f57615980552a93dfa796b9a7dbd3fbda4
samrath2007 <samrath1324@gmail.com>
2021-10-25 16:40:20 +0530

    fix(services): verify jwt token

--- services/auth/login.ts
+++ services/auth/login.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import jwt from "jsonwebtoken";
 
 interface Credentials {
   email: string;
@@ -14,36 +15,33 @@ interface Response {
 }
 
 const login = async (credentials: Credentials) => {
-  if (localStorage.getItem("userToken")) {
+  const userToken = localStorage.getItem("userToken");
+  if (userToken && jwt.verify(userToken, process.env.JWT_SECRET as string)) {
     return "logged-in";
-  } else {
-    try {
-      const res: Response = await axios.post(
-        `${process.env.API_URL}/auth/login`,
-        credentials
-      );
-      if (res.status === 200) {
-        if (res.data && res.data.token) {
-          localStorage.setItem("userToken", res.data.token);
-          return "logged-in";
-        } else {
-          return "internal-server-error";
-        }
-      } else if (res.status === 404) {
-        return "incorrect-username-or-password";
-      } else if (
-        res.status === 400 &&
-        res.message === "Token can't be created"
-      ) {
-        return "data-serializing-error";
+  }
+  try {
+    const res: Response = await axios.post(
+      `${process.env.API_URL}/auth/login`,
+      credentials
+    );
+    if (res.status === 200) {
+      if (res.data && res.data.token) {
+        localStorage.setItem("userToken", res.data.token);
+        return "logged-in";
       } else {
         return "internal-server-error";
       }
-    } catch (err) {
-      return err;
-    } finally {
-      return "failed-login";
+    } else if (res.status === 404) {
+      return "incorrect-username-or-password";
+    } else if (res.status === 400 && res.message === "Token can't be created") {
+      return "data-serializing-error";
+    } else {
+      return "internal-server-error";
     }
+  } catch (err) {
+    return err;
+  } finally {
+    return "failed-login";
   }
 };
 
