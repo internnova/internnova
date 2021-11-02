d6acb9593f180044893bc66f07e3bfc8e91eb8c1
samrath2007 <samrath1324@gmail.com>
2021-11-02 17:52:28 +0530

    feat(ui): loading component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { useUser } from "@auth0/nextjs-auth0";
 import { Landing } from "../components/HomePage/Unauthorized";
+import { Loading } from "../components/Loading";
 
 const Index = () => {
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
+  if (isLoading) return <Loading />;
   return <Landing user={user ? user : null} />;
 };
 
