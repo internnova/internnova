283dd27c594deb039ba890280b3263873f19adfd
samrath2007 <samrath1324@gmail.com>
2021-11-07 02:21:21 +0530

    feat(api): routes for company, intern and user creation

--- pages/api/db/createCompany.ts
+++ pages/api/db/createCompany.ts
@@ -1,5 +1,43 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma";
+import { Prisma } from "@prisma/client";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
+type CompanyCreate = {
+  userId: number;
+  name: string;
+  description: string;
+  logo: string;
+  website: string;
+  CIN: string;
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(400).send({ message: "Only POST requests allowed" });
+    return;
+  } else {
+    try {
+      const body: CompanyCreate = JSON.parse(req.body);
+      const company = await prisma.company.create({
+        data: {
+          ...body,
+        },
+      });
+      res
+        .status(200)
+        .json({ message: "Successfully created company", data: { company } });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (e.code === "P2002") {
+          res.status(400).json({
+            message:
+              "There is a unique constraint violation, a new company cannot be created with this data",
+            fields: e.meta,
+          });
+        }
+      }
+      throw e;
+    }
+  }
 };
--- pages/api/db/createIntern.ts
+++ pages/api/db/createIntern.ts
@@ -1,5 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../lib/prisma";
+import { Prisma, Tag } from "@prisma/client";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "John Doe" });
+type InternCreate = {
+  userId: number;
+  bio: string;
+  interests: Tag[];
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(400).send({ message: "Only POST requests allowed" });
+    return;
+  } else {
+    try {
+      const body: InternCreate = JSON.parse(req.body);
+      const intern = await prisma.intern.create({
+        data: {
+          ...body,
+        },
+      });
+      res
+        .status(200)
+        .json({ message: "Successfully created intern", data: { intern } });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (e.code === "P2002") {
+          res.status(400).json({
+            message:
+              "There is a unique constraint violation, a new intern cannot be created with this data",
+            fields: e.meta,
+          });
+        }
+      }
+      throw e;
+    }
+  }
 };
