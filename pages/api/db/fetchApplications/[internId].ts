f170540fd576f0eed721fb6365bf128335b6e1c2
samrath2007 <samrath1324@gmail.com>
2021-11-27 13:57:24 +0530

    fix(api): insanely stupid naming bug

--- pages/api/db/fetchApplications/[internId].ts
+++ pages/api/db/fetchApplications/[internId].ts
@@ -5,14 +5,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { internId } = req.query;
   if (!internId || parseInt(internId as string) === NaN) {
     // if there is no interest or the interest is not a valid tag return an error
-    res.status(400).json({ error: "Invalid or missing interest" });
+    res.status(400).json({ error: "Invalid or missing internId" });
     return;
   } else {
     const jobApplications = await prisma.jobApplication.findMany({
       where: { internId: parseInt(internId as string) },
     });
 
-    res.status(200).json({ application: jobApplications || [] });
+    res.status(200).json({ applications: jobApplications || [] });
     return;
   }
 };
