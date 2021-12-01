df273bb00b55a76061800cc19583f9d386ff97a2
samrath2007 <samrath1324@gmail.com>
2021-12-02 04:57:39 +0530

    feat(apply): success page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import InternHomepage from "../components/HomePage/Intern";
 import { Landing } from "../components/HomePage/Unauthorized";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 
 const Index = () => {
+  const router = useRouter();
+
+  const success = router.query.success ? true : false;
   return (
     <>
       <SignedIn>
-        <InternHomepage />
+        <InternHomepage success={success} />
       </SignedIn>
       <SignedOut>
         <Landing />
