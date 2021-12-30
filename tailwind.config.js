510ec2ace07f1f5124ed048427c1296336b96e1b
samrath <samrath.2757@gmail.com>
2021-12-30 13:26:36 +0530

    refactor: src/ folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
