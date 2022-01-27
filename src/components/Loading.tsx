3e2299f74dae1111cf5f1d2167c0230cc02894cf
Samrath Chadha <samrath.2757@gmail.com>
2022-01-27 10:44:53 +0530

    feat(loader): cool loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,11 @@
+import { SpinningOrbitLoader } from "react-loaders-kit";
 const Loading = () => {
   // very simple spinner
   return (
     <section className="flex h-screen">
-      <img
-        className="m-auto my-auto"
-        src="/assets/img/loading.gif"
-        alt="screenshot"
-      />
+      <div className="my-auto mx-auto">
+        <SpinningOrbitLoader size={150} loading />
+      </div>
     </section>
   );
 };
