3ce0d5fd316d70f14b0627e6b246683ec67a3247
Samrath Chadha <samrath.2757@gmail.com>
2022-02-22 22:47:30 +0530

    feat(spinner): new spinner

--- app/core/components/Spinner.tsx
+++ app/core/components/Spinner.tsx
@@ -1,5 +1,11 @@
-export const Spinner = () => (
-  <div className="grid overflow-hidden place-items-center w-full h-screen">
-    <div className="loader" />
-  </div>
-)
+import {SpinningOrbitLoader} from "react-loaders-kit"
+export const Spinner = () => {
+  // very simple spinner
+  return (
+    <section className="flex h-screen">
+      <div className="my-auto mx-auto">
+        <SpinningOrbitLoader size={150} loading />
+      </div>
+    </section>
+  )
+}
