a61d852e7a260d68cfa89c7227e9835f63e98427
Samrath Chadha <samrath.2757@gmail.com>
2022-05-02 08:49:28 +0530

    Fix job page button hidden bug

--- app/pages/jobs/[companyName]/[slug]/index.tsx
+++ app/pages/jobs/[companyName]/[slug]/index.tsx
@@ -1,5 +1,5 @@
-import { Button } from "app/core/components/Button"
-import { Spinner } from "app/core/components/Spinner"
+import {Button} from "app/core/components/Button"
+import {Spinner} from "app/core/components/Spinner"
 import Layout from "app/core/layouts/Layout"
 import deleteJob from "app/jobs/mutations/deleteJob"
 import getJob from "app/jobs/queries/getJob"
@@ -14,14 +14,14 @@ import {
   useRouter,
   useSession,
 } from "blitz"
-import { Suspense } from "react"
+import {Suspense} from "react"
 
 export const Job = () => {
   const router = useRouter()
   const slug = useParam("slug", "string")
   const companyName = useParam("companyName", "string")
   const [deleteJobMutation] = useMutation(deleteJob)
-  const [job] = useQuery(getJob, { slug, companyName })
+  const [job] = useQuery(getJob, {slug, companyName})
   const session = useSession()
 
   if (job) {
@@ -43,7 +43,7 @@ export const Job = () => {
             {position} @{companyName}
           </title>
         </Head>
-        <div className="h-[90vh]">
+        <div>
           <div className="profile pt-16">
             <div className="relative mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 flex flex-col gap-10">
               <h1>
@@ -103,7 +103,7 @@ export const Job = () => {
                 !job.applications
                   .map((application) => application.internId)
                   .includes(session?.userId || NaN) && (
-                  <Link href={Routes.NewJobApplicationPage({ jobSlug: job.slug })}>
+                  <Link href={Routes.NewJobApplicationPage({jobSlug: job.slug})}>
                     <a className="flex gap-2 items-center">
                       <p className="text-lg">Looks good?</p>
                       <Button options="px-2">Apply Now!</Button>
