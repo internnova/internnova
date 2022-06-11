ada94aa5f95a12a35766dd0548f3968862eb715c
Samrath Chadha <samrath.2757@gmail.com>
2022-06-11 18:02:19 +0530

    fix(job): check against company id for auth instead of job id

--- app/jobs/mutations/deleteJob.ts
+++ app/jobs/mutations/deleteJob.ts
@@ -1,22 +1,19 @@
-import { resolver, Ctx, AuthenticationError } from "blitz"
+import {resolver, Ctx, AuthenticationError} from "blitz"
 import db from "db"
-import { z } from "zod"
+import {z} from "zod"
 
 const DeleteJob = z.object({
   id: z.number(),
 })
 
-export default resolver.pipe(
-  resolver.zod(DeleteJob),
-  resolver.authorize(),
-  async ({ id }, ctx: Ctx) => {
-    ctx.session.$authorize("COMPANY")
-    if (ctx.session.userId === id) {
-      const job = await db.job.deleteMany({ where: { id } })
+export default resolver.pipe(resolver.zod(DeleteJob), async ({id}, ctx: Ctx) => {
+  ctx.session.$authorize("COMPANY")
+  const job = await db.job.findFirst({where: {id}})
+  if (ctx.session.userId === job?.companyId) {
+    const deletedJob = await db.job.deleteMany({where: {id}})
 
-      return job
-    } else {
-      throw new AuthenticationError("You are not authorized to delete this job")
-    }
+    return deletedJob
+  } else {
+    throw new AuthenticationError("You are not authorized to delete this job")
   }
-)
+})
